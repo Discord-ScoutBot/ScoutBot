41948f836341a37640e4ea0a6c2700a96ebd3540
SpikeyScout <34626346+SpikeyScout@users.noreply.github.com>
2020-03-31 22:15:31 -0400

    Add basic ping

--- rewrite.js
+++ rewrite.js
@@ -6,5 +6,13 @@ client.on('ready', () => {
     client.user.setPresence({ game: { name: 'scoutbot! rewritten! |help', type: 2 } });
     console.log(`Logged in as ${client.user.tag}!`);
   }); 
+  if (message.content === '|ping') {
+    if (message.author.bot) return;
+    console.log(`Someone requested a ping! Currently at ${msg.createdTimestamp - message.createdTimestamp}`)
+    message.channel.send('Checking ping!...')
+    .then(msg => {
+        msg.edit(`Current Ping: ${msg.createdTimestamp - message.createdTimestamp}ms.`);
+    });
+}
   client.login(config.token)
 // no token for you, buddy!
