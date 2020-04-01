fd0a5cd7fac493f5ebe7ef52819be2915a82a09a
SpikeyScout <34626346+SpikeyScout@users.noreply.github.com>
2020-03-31 22:33:08 -0400

    basic ping command

--- rewrite.js
+++ rewrite.js
@@ -1,18 +1,20 @@
-// im pretty sure a bot needs to login first...
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
 client.on('ready', () => {
-    client.user.setPresence({ game: { name: 'scoutbot! rewritten! |help', type: 2 } });
+    client.user.setPresence({ game: { name: 'scoutbot - reborn |help', type: 2 } });
     console.log(`Logged in as ${client.user.tag}!`);
   }); 
+  client.on("message", message => {
+
   if (message.content === '|ping') {
     if (message.author.bot) return;
-    console.log(`Someone requested a ping! Currently at ${msg.createdTimestamp - message.createdTimestamp}`)
+    console.log(`Someone requested a ping!`)
     message.channel.send('Checking ping!...')
     .then(msg => {
         msg.edit(`Current Ping: ${msg.createdTimestamp - message.createdTimestamp}ms.`);
     });
 }
+});
   client.login(config.token)
-// no token for you, buddy!
+// no token for you, buddy
