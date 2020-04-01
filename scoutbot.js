b1bbcfe31cf3438cbbc8ee4ecde4a55c0443709c
Sate04 <50961365+Sate04@users.noreply.github.com>
2020-04-01 00:07:26 -0400

    Update scoutbot.js

--- scoutbot.js
+++ scoutbot.js
@@ -1,6 +1,20 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function flip(){
+  coin = getRandomInt(1);
+  if(coin === 0){
+    return true;
+  } else {
+    return false;
+  }
+}
+
 client.on('ready', () => {
     client.user.setPresence({ activity: { name: 'scoutbot! reborn!' }, status: 'idle' })
     console.log(`Logged in as ${client.user.tag}!`);
@@ -16,12 +30,17 @@ client.on('ready', () => {
 }
 // funny stuff hahahaha
 if (message.content === "|iq") {
-  function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-  }
   message.channel.send(`Hmm... let me guess your IQ <@${message.author.id}>.`)
-  message.reply(`your IQ is ${getRandomInt(300)}!`);
+  message.reply(`your IQ is ${getRandomInt(263)}!`);
 }
+if (message.content === "|coin"){
+  if(flip){
+    message.channel.send('Heads!')
+  } else {
+    message.channel.send('Tails!')
+  }
+}
+  
 // misc stuff
 const pfpEmbed = {
 	title: `${message.author.username}'s profile picture`,
