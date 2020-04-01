11f085bd2f8fced9f9a0ae08960b0aabde3938ee
SpikeyScout <34626346+SpikeyScout@users.noreply.github.com>
2020-03-31 23:30:03 -0400

    rename rewrite.js to scoutbot.js

---
+++ scoutbot.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
+client.on('ready', () => {
+    client.user.setPresence({ activity: { name: 'scoutbot! reborn!' }, status: 'idle' })
+    console.log(`Logged in as ${client.user.tag}!`);
+  }); 
+  client.on("message", message => {
+
+  if (message.content === '|ping') {
+    if (message.author.bot) return;
+    message.channel.send('Checking ping!...')
+    .then(msg => {
+        msg.edit(`Current Ping: ${msg.createdTimestamp - message.createdTimestamp}ms.`);
+    });
+}
+const pfpEmbed = {
+	title: `${message.author.username}'s profile picture`,
+	image: {
+		url: message.author.displayAvatarURL(),
+	},
+};
+    if (message.content === '|pfp') {
+      message.reply("here is your profile picture!")
+      message.channel.send({embed: pfpEmbed });
+    }
+
+});
+  client.login(config.token)
+// no token for you, buddy
