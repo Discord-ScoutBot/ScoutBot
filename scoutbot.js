ac5e5888d43a96023044479b105b98e73eecb07a
Authomc <63015852+Authomc@users.noreply.github.com>
2020-04-01 15:11:02 -0400

    change formatting

--- scoutbot.js
+++ scoutbot.js
@@ -1,58 +1,66 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
+const PREFIX = "|";
 
 function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
+    return Math.floor(Math.random() * Math.floor(max));
 }
 
-function flip(odds){
-  coin = getRandomInt(odds);
-  if(coin > 5){
-    return true;
-  } else {
-    return false;
-  }
+function flip(odds) {
+    coin = getRandomInt(odds);
+    if (coin > 5) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 client.on('ready', () => {
     client.user.setPresence({ activity: { name: 'scoutbot! reborn!' }, status: 'idle' })
     console.log(`Logged in as ${client.user.tag}!`);
-  }); 
-  client.on("message", message => {
-  // stats n stuff woooooo
-  if (message.content === '|ping') {
-    if (message.author.bot) return;
-    message.channel.send('Checking ping!...')
-    .then(msg => {
-        msg.edit(`Current Ping: ${msg.createdTimestamp - message.createdTimestamp}ms.`);
-    });
-}
-// funny stuff hahahaha
-if (message.content === "|iq") {
-  message.channel.send(`Hmm... let me guess your IQ <@${message.author.id}>.`)
-  message.reply(`your IQ is ${getRandomInt(250)}!`);
-}
-if (message.content === "|flip"){
-  if(flip(10)){
-    message.channel.send('Heads!')
-  } else {
-    message.channel.send('Tails!')
-  }
-}
-  
-// misc stuff
-const pfpEmbed = {
-	title: `${message.author.username}'s profile picture`,
-	image: {
-		url: message.author.displayAvatarURL(),
-	},
-};
-    if (message.content === '|pfp') {
-      message.reply("here is your profile picture!")
-      message.channel.send({embed: pfpEmbed });
-    }
-
 });
-  client.login(config.token)
-// no token for you, buddy
+
+client.on("message", message => {
+    //If the message was sent by a bot, ignore it
+    if (message.author.bot) return;
+
+    //Define necessary values
+    let pfpEmbed = {
+        title: `${message.author.username}'s profile picture`,
+        image: {
+            url: message.author.displayAvatarURL(),
+        },
+    };
+    let args = message.content.substring(PREFIX.length).split(" ");
+
+    //If the first character of the message isn't the prefix, return
+    if (message.content.slice(0, 1) !== PREFIX) return;
+
+    switch (args[0]) {
+        case "ping":
+            message.channel.send('Checking ping!...')
+                .then(msg => {
+                    msg.edit(`Current Ping: ${msg.createdTimestamp - message.createdTimestamp}ms.`);
+                });
+            break;
+        case "iq":
+            message.channel.send(`Hmm... let me guess your IQ <@${message.author.id}>.`)
+            message.reply(`your IQ is ${getRandomInt(250)}!`);
+            break;
+        case "flip":
+            if (flip(10)) {
+                message.channel.send('Heads!')
+            } else {
+                message.channel.send('Tails!')
+            }
+            break;
+        case "pfp":
+            message.reply("here is your profile picture!")
+            message.channel.send({embed: pfpEmbed });
+            break;
+    }
+});
+
+client.login(config.token)
+
